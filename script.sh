b34dadce816e9a6513ca28940fd37ed19eb3e86d
Indra7946 <44440773+Indra7946@users.noreply.github.com>
2022-09-18 17:57:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 #! /bin/sh
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ec2-user@54.147.46.229 "sh /home/ec2-user/Monitoring/Healthcheck_Monitoring.sh"
-scp -i ~/.ssh/id_rsa ec2-user@54.147.46.229:/home/ec2-user/Monitoring/*.html .
+scp -i ~/.ssh/id_rsa ec2-user@54.147.46.229:/home/ec2-user/Monitoring/Healthcheck_Report*.html .
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ec2-user@54.147.46.229 "rm -rf /home/ec2-user/Monitoring/Healthcheck_Report*.html"
 
