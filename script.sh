6d27fbb7a35be7b4cd424ad2ef674d4bd20f740c
EC2 Default User <ec2-user@ip-172-31-37-56.ap-south-1.compute.internal>
2023-01-08 13:24:22 +0000

    script edited

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 
 ## This is my script
 
-echo "This script is written by Indra"
+echo "This script is written by Indra update"
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ec2-user@54.147.46.229 "sh /home/ec2-user/Monitoring/Healthcheck_Monitoring.sh"
 scp -i ~/.ssh/id_rsa ec2-user@54.147.46.229:/home/ec2-user/Monitoring/Healthcheck_Report*.html .
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ec2-user@54.147.46.229 "rm -rf /home/ec2-user/Monitoring/Healthcheck_Report*.html"
