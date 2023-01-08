19957b91738d4a48f001eb4a920518f2032d9083
EC2 Default User <ec2-user@ip-172-31-37-56.ap-south-1.compute.internal>
2023-01-08 13:37:47 +0000

    latest edit after cred helper

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #! /bin/sh
 
-## This is my script
+## This is my script addition
 
 echo "This script is written by Indra updated latest"
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ec2-user@54.147.46.229 "sh /home/ec2-user/Monitoring/Healthcheck_Monitoring.sh"
